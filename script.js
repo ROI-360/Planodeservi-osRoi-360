9f6db18f44ab7bc9d1c7d51656b8a6b93543c625
ROI-360 <Laurindomatheus40@gmail.com>
2025-06-27 14:00:10 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const meuBotao = document.getElementById('meuBotao');
-
-    meuBotao.addEventListener('click', () => {
-        alert('Olá! Você clicou no botão!');
-    });
-});
